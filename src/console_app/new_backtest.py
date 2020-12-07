b77564fd14d70c995f2e26c530b1e35670d9432f
Sebastian Flum <sebastian.flum@hotmail.de>
2020-12-07 19:07:24 +0100

    Added frame for first steps of the console application

---
+++ src/console_app/new_backtest.py
@@ -0,0 +1,15 @@
+# Choose API
+
+# Choose currency
+
+# Choose strategy
+
+# Enter starting capital
+
+# Enter buy quantity
+
+# Enter start date (optional)
+
+# Enter end date (optional)
+
+# Enter name?
