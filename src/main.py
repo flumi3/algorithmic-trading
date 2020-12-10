2e6b5788f7f68168a4025ab219af037877dbbaee
Sebastian Flum <sebastian.flum@hotmail.de>
2020-12-10 13:33:42 +0100

    adjusted main file to the modified functions

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
-import os
-import logger  # Don't delete. Executes the log config
-
 from backtest.backtest import Backtest
 from api.binance import Binance
-from typing import Dict
+from collections import OrderedDict
 from market_data import MarketData
 from strategies.moving_average_strategy import MovingAverageStrategy
 from uuid import UUID
 from backtest.signals import BuySignal, SellSignal
-from datetime import datetime
+from plotly.graph_objs import Figure
 
 
 def main() -> None:
@@ -21,8 +18,8 @@ def main() -> None:
 
     # Run strategy on market data
     strategy: MovingAverageStrategy = MovingAverageStrategy()
-    buy_signals: Dict[UUID, BuySignal] = strategy.calc_buy_signals(market_data.candlestick_data)
-    sell_signals: Dict[UUID, SellSignal] = strategy.calc_sell_signals(market_data.candlestick_data, buy_signals)
+    buy_signals: OrderedDict[UUID, BuySignal] = strategy.calc_buy_signals(market_data.candlestick_data)
+    sell_signals: OrderedDict[UUID, SellSignal] = strategy.calc_sell_signals(market_data.candlestick_data, buy_signals)
 
     # Backtest
     api: str = binance.name
@@ -30,17 +27,14 @@ def main() -> None:
     capital: float = 50000.0
     quantity: float = 1.0
     transaction_fee: float = binance.TRADING_FEE
-    start_date: datetime = datetime.now()  # TODO: set real date
-    end_date: datetime = datetime.now()  # TODO: set real date
 
-    root_dir: str = os.path.dirname(os.path.abspath("Algorithmic-Trading"))
-    plot_title: str = root_dir + "/" + symbol
-
-    backtest: Backtest = Backtest(symbol, api, strategy_name, capital, quantity, transaction_fee, start_date, end_date,
-                                  market_data, buy_signals=buy_signals, sell_signals=sell_signals)
+    backtest: Backtest = Backtest(symbol, api, strategy_name, capital, quantity, transaction_fee, market_data,
+                                  buy_signals=buy_signals, sell_signals=sell_signals)
     backtest.run_backtest()
-    backtest.plot_candlestick_chart(plot_title)
-    backtest.plot_capital_chart()
+
+    candlestick_fig: Figure = backtest.create_candlestick_figure()
+    capital_fig: Figure = backtest.create_capital_figure()
+    backtest.figures_to_html([candlestick_fig, capital_fig])
 
 
 if __name__ == "__main__":
