336ab03863e105bb6a712110e82a24d719b3cff5
Sebastian Flum <sebastian.flum@hotmail.de>
2020-12-10 13:34:31 +0100

    Added class for easy access of color codes for colored print statements

--- src/util.py
+++ src/util.py
@@ -20,6 +20,22 @@ class BinanceAPIException(Exception):
         return 'APIError(code=%s): %s' % (self.code, self.message)
 
 
+class TerminalColors:
+    """
+    Colors for colored print outputs.
+    Usage: print(OKCYAN + "Text that will be colored" + ENDC)
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 # TODO: evaluate
 def datestr_to_timestamp(datestr: str) -> int:
     """
