359f5fa55d1661bc938c914900ac52d6a866b7c7
Sebastian Flum <sebastian.flum@hotmail.de>
2021-01-22 11:45:56 +0100

    recycled market data to only be in use with the live trading (bot)

--- src/market_data.py
+++ src/market_data.py
@@ -1,21 +1,36 @@
 import logging
+from datetime import datetime
 
 from logging import Logger
 from typing import List
+
 from pandas import DataFrame
-from indicators import Indicator, SmoothedMovingAverage
 
 logger: Logger = logging.getLogger("__main__")
 
 
 class MarketData:
 
-    def __init__(self, data: DataFrame) -> None:
+    def __init__(self, symbol: str, init_times: List[datetime], init_prices: List[float]) -> None:
         logger.info("Creating new market data...")
-        self.candlestick_data: DataFrame = data
-        self.indicators: List[Indicator] = list()  # List of indicators to keep track of the contained indicators
+        self.symbol: str = symbol
+        self.times: List[datetime] = init_times
+        self.prices: List[float] = init_prices
 
-    def add_sma(self, name: str, period: int) -> None:
-        sma = SmoothedMovingAverage(name, period)  # Create new indicator
-        sma.add_data(self.candlestick_data)  # Add indicator data to the candlestick data
-        self.indicators.append(sma)
+    def add_entry(self, time: datetime, price: float):
+        # Append entry to the end
+        self.times.append(time)
+        self.prices.append(price)
+        # Remove oldest entry to keep the same amount of entries
+        self.times.pop(0)
+        self.prices.pop(0)
+
+    def create_dataframe(self) -> DataFrame:
+        return DataFrame(list(zip(self.times, self.prices)), columns=["time", "price"])
+
+
+# market data mit einem data property das einen dataframe hält. dataframe enthält bestimmte feste anzahl an entries,
+# welche man vorher angeben muss. entries bestehen nur aus zeit und preis. es gibt dann eine add entry methode die
+# einen neuen entry hinten anhängt und den ältesten entry aus dem df löscht, um das limit einzuhalten. dann kann man
+# immer das gleiche market data object verwalten und auf kopien dessen die indicator anwenden und check ob der neueste
+# entry die strat conditions matched
